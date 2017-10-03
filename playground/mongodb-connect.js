b633ced561b4c8acfd3116f1eb3d3762ef6b1496
guru dayal <gurudayal37@yahoo.in>
2017-10-04 04:01:40 +0530

    Add post /todos route

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -16,15 +16,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
   //   console.log(JSON.stringify(result.ops,undefined,2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Guru dayal',
-    age:20,
-    location:'India'
-  },(err,result)=>{
-    if(err){
-      return console.log('Unable to insert user ',err);
-    }
-    console.log(result.ops[0]._id.getTimestamp());
-  });
-  db.close();
-});
+//   db.collection('Users').insertOne({
+//     name: 'Guru dayal',
+//     age:20,
+//     location:'India'
+//   },(err,result)=>{
+//     if(err){
+//       return console.log('Unable to insert user ',err);
+//     }
+//     console.log(result.ops[0]._id.getTimestamp());
+//   });
+//   db.close();
+//  });
