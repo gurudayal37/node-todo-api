9536d323845e13107a832823a6746971e874f178
guru dayal <gurudayal37@yahoo.in>
2017-10-09 04:11:11 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -56,6 +57,31 @@ app.post('/todos', (req, res) => {
      }
     res.status(200).send({todo});
    }).catch((e)=>res.status(400).send());
+ });
+
+ app.patch('/todos/:id',(req,res)=>{
+   var id=req.params.id;
+   var body=_.pick(req.body,['text','completed']);
+
+   if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+   }
+
+   if(_.isBoolean(body.completed) && body.completed){
+     body.completedAt=new Date().getTime();
+   }else {
+     body.completedAt=null;
+     body.completed=false;
+   }
+
+   Todo.findByIdAndUpdate(id,{$set : body},{new : true}).then((todo)=>{
+     if(!todo){
+       return res.status(404).send();
+     }
+     res.send(todo);
+   }).catch((e)=>{
+     res.status(400).send();
+   });
  });
 
 app.listen(port, () => {
