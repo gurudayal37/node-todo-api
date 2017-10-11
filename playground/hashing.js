8a987467665095057dcb6f1f544a20c2ef891fc9
guru dayal <gurudayal37@yahoo.in>
2017-10-11 21:55:39 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-  id:10
-};
+var passward='123abc!';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(passward,salt,(err,hash)=>{
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded ',decoded);
+var hashedPassword='$2a$10$CD.nvhPS2RB/tiR5oTJ/u.CwpqxD8.9KKcLUSEMdz4UPzAbfDF5ve';
+
+bcrypt.compare('123ab!',hashedPassword,(err,res)=>{
+  console.log(res);
+});
+// var data={
+//   id:10
+// };
+//
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded ',decoded);
 
 // var message = 'I am user no 4';
 // var hash = SHA256(message).toString();
